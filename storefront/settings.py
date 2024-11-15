ae0033a3f943eba9512e44b6949d4f58d52fd856
dmurawski <dmurawski018@gmail.com>
2024-11-15 14:06:39 +0100

    add apps likes,store,tags and add dummies data, change database to mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "debug_toolbar",
     # local apps
     "playground.apps.PlaygroundConfig",
+    "store.apps.StoreConfig",
+    "tags.apps.TagsConfig",
+    "likes.apps.LikesConfig",
 ]
 
 MIDDLEWARE = [
@@ -78,10 +81,20 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "storefront",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "root",
     }
 }
 
