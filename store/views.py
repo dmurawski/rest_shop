af52dbc6234849cc954a66fcf8bcfcbca39e639f
dmurawski <dmurawski018@gmail.com>
2024-11-18 12:46:51 +0100

    add settings to rest decimal_to_string:False, add serializers to api view

--- store/views.py
+++ store/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view()
 def product_list(request):
-    return Response("hello")
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
--- storefront/settings.py
+++ storefront/settings.py
@@ -147,3 +147,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False,
+}
