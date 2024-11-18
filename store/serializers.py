0b7a010792057ce061dbb740670617fd884a7fc6
dmurawski <dmurawski018@gmail.com>
2024-11-18 14:50:46 +0100

    saving objects create,update

--- store/serializers.py
+++ store/serializers.py
@@ -15,6 +15,9 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "title",
+            "description",
+            "slug",
+            "inventory",
             "unit_price",
             "price_with_tax",
             "collection",
@@ -26,3 +29,14 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get("unit_price")
+    #     instance.save()
+    #     return instance
