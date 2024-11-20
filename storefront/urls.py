eacd163512bdf042dee7b00411b0ca0f229f6831
dmurawski <dmurawski018@gmail.com>
2024-11-20 16:43:39 +0100

    add settings to media files

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,6 +1,8 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "Storefront Admin"
 admin.site.index_title = "Admin"
@@ -12,3 +14,9 @@ urlpatterns = [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
